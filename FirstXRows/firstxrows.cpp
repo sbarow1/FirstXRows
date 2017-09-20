6c5876b56020a9cbbc7bc6f49cc94f7e33ae5b70
sbarow <sbarow@gmail.com>
2017-09-20 11:03:57 -0400

    Added functionality to get the correct num of rows and update the master file. Pretty much done.

--- FirstXRows/firstxrows.cpp
+++ FirstXRows/firstxrows.cpp
@@ -1,6 +1,10 @@
 #include "firstxrows.h"
 #include <iostream>
 #include <QFileDialog>
+#include "CSVLibrary.h"
+#include <fstream>
+#include <cstdlib>
+#include <string>
 
 FirstXRows::FirstXRows(QWidget *parent)
 	: QMainWindow(parent)
@@ -10,7 +14,49 @@ FirstXRows::FirstXRows(QWidget *parent)
 
 void FirstXRows::on_okButton_clicked()
 {
-	std::cout << "I am in on_okButton_clicked()" << std::endl;
+	// open input file
+	std::ifstream fin(masterFileName.toStdString());
+
+	// open output file
+	std::ofstream fout(outputFileName.toStdString());
+
+	// Read a row from the input file and output it to the output file until end of file
+	// or you have read the correct number of rows
+	CSVLibrary::CSVRow row;
+
+	int count = 0;
+	// vector for the rest of the rows so that we can update the Master file
+	std::vector<CSVLibrary::CSVRow> rows;
+	while (fin >> row)
+	{
+		if (count == 0)
+		{
+			fout << row;
+			rows.push_back(row);
+		}
+		else if (count < numLines)
+		{
+			fout << row;
+		} 
+		else 
+			rows.push_back(row);
+
+		count++;
+	}
+
+	// Close the input and output files
+	fin.close();
+	fout.close();
+
+	// Now update the Master File (remove the rows that you added to the outputfile
+	std::ofstream newMaster(masterFileName.toStdString());
+	for (auto i : rows)
+		newMaster << i;
+
+	newMaster.close();
+
+	// Quit the QT GUI application
+	QCoreApplication::quit();
 }
 
 void FirstXRows::on_masterToolButton_clicked()
